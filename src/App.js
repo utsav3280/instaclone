ad3d1a5005e34f41d7a6a26126e137569f76c8fb
Utsav <utsav.s3280@gmail.com>
2022-12-16 02:57:36 +0530

    added react UI

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "./components/homePage";
+import LandingPage from "./components/landingPage";
+import PostForm from "./components/postForm";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/home/createpost" element={<PostForm />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
